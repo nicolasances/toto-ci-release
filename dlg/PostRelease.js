15c3676f6686e976c770d6ef3d1bd731aaeb1797
nicolas <nicolas.matteazzi@gmail.com>
2018-08-03 21:20:58 +0200

    Removed log

--- dlg/PostRelease.js
+++ dlg/PostRelease.js
@@ -21,7 +21,7 @@ exports.do = function(conf) {
 
     // 2. Check what type of microservice it is
     // If it's the toto webapp (toto)
-    if (conf.microservice == 'toto') {releaseToto.do(conf).then(function() {console.log('finished'); success();}, failure);}
+    if (conf.microservice == 'toto') {releaseToto.do(conf).then(function() {success();}, failure);}
 
     // TODO: if it's a web application (generic toto-ui-)
     else if (conf.microservice.startsWith('toto-ui-')) {failure({error: 'Not implemented'});}
