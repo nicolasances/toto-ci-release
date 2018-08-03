5734f6fe5e1814b5e512075ccb1787b90af676d1
nicolas <nicolas.matteazzi@gmail.com>
2018-08-03 20:47:05 +0200

    Trying to resolve var name conflict

--- dlg/WriteTotoConfig.js
+++ dlg/WriteTotoConfig.js
@@ -2,38 +2,36 @@ var fs = require('fs');
 
 exports.do = function(data) {
 
-  var config = data;
-
   return new Promise(function(success, failure) {
 
     // 1. Retrieve microservice from GitHub
-    console.log('[' + config.microservice + '] - Creating config.js file...');
+    console.log('[' + data.microservice + '] - Creating config.js file...');
 
-    var protocol = config.ssl ? 'https' : 'http';
-    var port = config.ssl ? '443' : '80';
-    var host = config.host;
+    var protocol = data.ssl ? 'https' : 'http';
+    var port = data.ssl ? '443' : '80';
+    var host = data.host;
 
     // 2. Create the configuration file
-    var data = '';
+    var file = '';
 
-    data += 'var microservicesProtocol = "' + protocol + '"; \r\n';
-    data += 'var microservicesHost = "' + host + '"; \r\n';
-    data += 'var microservicesPort = "' + port + '"; \r\n';
-    data += 'var microservicesUrl = "' + host + '"; \r\n';
-    data += 'var microservicesUrl2 = "' + host + '"; \r\n';
+    file += 'var microservicesProtocol = "' + protocol + '"; \r\n';
+    file += 'var microservicesHost = "' + host + '"; \r\n';
+    file += 'var microservicesPort = "' + port + '"; \r\n';
+    file += 'var microservicesUrl = "' + host + '"; \r\n';
+    file += 'var microservicesUrl2 = "' + host + '"; \r\n';
 
     // Write
-    fs.writeFile('/' + config.microservice + '/toto/conf/config.js', data, function(err, data) {
+    fs.writeFile('/' + config.microservice + '/toto/conf/config.js', file, function(err, res) {
 
       if (err) {
-        console.log('[' + config.microservice + '] - Error creating the config.js file!');
+        console.log('[' + data.microservice + '] - Error creating the config.js file!');
         failure(err);
         return;
       }
 
-      console.log('[' + config.microservice + '] - config.js file created!');
+      console.log('[' + data.microservice + '] - config.js file created!');
 
-      success(config);
+      success(data);
 
     });
 
