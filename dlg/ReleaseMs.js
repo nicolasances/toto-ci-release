fcbae4aa2993b45d6f16101d350bbe5a67d42140
nicolas <nicolas.matteazzi@gmail.com>
2019-01-13 11:53:08 +0100

    Updating Tyk also for toto-ms microservices

--- dlg/ReleaseMs.js
+++ dlg/ReleaseMs.js
@@ -1,19 +1,24 @@
 var exec = require('child_process').exec;
 
-var downloadCode = require('./DownloadMSCode');
-var buildDockerImage = require('./DockerImageBuild');
-var pushDockerImage = require('./DockerImagePush');
 var runDockerImage = require('./DockerImageRun');
+var updateTyk = require('./TykUpdate');
 
 exports.do = function(data) {
 
   return new Promise(function(success, failure) {
+
+    data.name = data.microservice.substring('toto-ms-'.length);
 
     // For Java microservices, I'm not gonna compile and stuff... just get the dockerhub image..
     // Java microservices are not supported anymore.. too heavy
 
     // 1. Run docker image
     runDockerImage.do(data).then(() => {
+
+      // 2. Deploy on API Gateway
+      return updateTyk.do(data);
+
+    }).then(() => {
 
       success({microservice: data.microservice, deployed: true});
 
